855e36b657e4972e17158550a99b5fff8a472eb1
FouJiTaiLai <foujitailaimail@163.com>
2018-07-31 20:39:34 +0800

    重构

--- src/Game/_Egret/LoopImageLayer.ts
+++ src/Game/_Egret/LoopImageLayer.ts
@@ -1,5 +1,14 @@
 /**
  * 循环显示图片层
+ * let bg1 = new LoopImageLayer(100000, "Map01_json.Map01_Far");
+ * let bg2 = new LoopImageLayer(50000, "Map01_json.Map01_Mid");
+ * let bg3 = new LoopImageLayer(15000, "Map01_json.Map01_Near");
+ * let bg4 = new LoopImageLayer(10000, "Map01_json.Map01_Ground");
+ * bgl.addChild(bg1);
+ * bgl.addChild(bg2);
+ * bgl.addChild(bg3);
+ * bgl.addChild(bg4);
+ * bg4.y = stage.stageHeight - 300;
  */
 class LoopImageLayer extends egret.DisplayObjectContainer implements IDisposable
 {
@@ -30,32 +39,6 @@ class LoopImageLayer extends egret.DisplayObjectContainer implements IDisposable
         egret.Tween.get(this, {loop: true})
             .to({x: 0}, 0)
             .to({x: -textureWidth}, textureSpeed);
-    }
-
-    public Dispose(): void
-    {
-        egret.Tween.get(this).pause();
-        this.removeChildren();
-    }
-
-}
-/**
- * 循环显示显示对象
- */
-class LoopImage extends egret.DisplayObjectContainer implements IDisposable
-{
-    constructor(speed: number, loopUnit: number, disObj:egret.DisplayObject)
-    {
-        super();
-
-        this.addChild(disObj);
-
-        let stageWidth = egret.MainContext.instance.stage.stageWidth;
-
-        let textureSpeed = loopUnit / stageWidth * speed;
-        egret.Tween.get(this, {loop: true})
-            .to({x: 0}, 0)
-            .to({x: -loopUnit}, textureSpeed);
     }
 
     public Dispose(): void
